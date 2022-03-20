3446b2b3b42dad57835ee85de70bac5e501e2947
Bubble-Tea-Li <74436917+Bubble-Tea-Li@users.noreply.github.com>
2022-03-19 21:50:13 -0700

    implement the boolean from test

---
+++ test2-1/src/main/java/seth/li/Args.java
@@ -0,0 +1,21 @@
+package seth.li;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Parameter;
+import java.util.Arrays;
+import java.util.List;
+
+public class Args {
+    public static <T> T parse(Class<T> optionsClass, String... args) throws Exception {
+        try {
+            Constructor<?> constructor = optionsClass.getDeclaredConstructors()[0];
+            Parameter parameter = constructor.getParameters()[0];
+            Option option = parameter.getAnnotation(Option.class);
+            List<String> arguments = Arrays.asList(args);
+
+            return (T) constructor.newInstance(arguments.contains("-" + option.value()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- test2-1/src/test/java/seth/li/ArgsTest.java
+++ test2-1/src/test/java/seth/li/ArgsTest.java
@@ -1,17 +1,51 @@
 package seth.li;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class ArgsTest {
-    @Test
-    public void should() {
-        Arguments args = Args.Parse("l:b,p:d,d:s", "-l", "-p", "8080", "-d", "/usr/logs");
-        args.getBool("l");
-        args.getInt("p");
+import static org.junit.jupiter.api.Assertions.*;
 
-        Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
-        options.logging();
-        options.port();
+public class ArgsTest {
+    // -l -p 8080 -d /usr/logs
+    // Single Option
+    // TODO:    -boolean -k
+    @Test
+    public void should_set_boolean_option_to_true_if_flag_present() throws Exception {
+        BooleanOption option = Args.parse(BooleanOption.class, "-l");
+
+        assertTrue(option.logging());
+    }
+
+    @Test
+    public void should_set_boolean_option_to_true_if_flag_not_present() throws Exception {
+        BooleanOption option = Args.parse(BooleanOption.class);
+
+        assertFalse(option.logging());
+    }
+
+    static record BooleanOption(@Option("l")Boolean logging) {}
+
+    // TODO:    -int -p 8080
+    // TODO:    -string -d /usr/logs
+    // TODO: Multiple Options: -l -p 8080 -d /usr/logs
+    // sad path:
+    // -boolean -l t // -l t f --> should fail
+    // -int -p / -p 8080 8081
+    // -string -d / -d /usr/logs /usr/vars
+    // default value
+    // - bool : false
+    // - int: 0
+    // - string: ""
+
+
+
+    @Test
+    @Disabled
+    public void should_behave() throws Exception {
+        Options options = Args.parse(Options.class, "-l", "-p", "8080", "-d", "/usr/logs");
+        assertTrue(options.logging());
+        assertEquals(8080, options.port());
+        assertEquals("usr/logs", options.directory());
     }
 
     static record Options(@Option("l")boolean logging, @Option("p")int port, @Option("d")String directory) {}
